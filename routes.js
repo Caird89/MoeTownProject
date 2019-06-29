9cb4d7514b3ef78b2e000c96c227aadfd4e85f30
Caird89 <32108275+Caird89@users.noreply.github.com>
2019-06-29 01:48:19 -0400

    changes

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const pagesRoutes = require('./routes/pages')
+const pagesRoutes = require('./routes/pages');
 const tasksRoutes = require('./routes/tasks');
 const authorsRoutes = require('./routes/authors');
 const sessionsRoutes = require('./routes/sessions');
